97541af0d4bf3768d74a3c3cb3349147ec949432
santosh.pasupunuri <santosh.pasupunuri@vistex.com>
2018-06-12 19:29:44 +0530

    modules addtion

--- controller/App.controller.js
+++ controller/App.controller.js
@@ -1,16 +1,11 @@
 sap.ui.define([
-	"sap/ui/core/mvc/Controller"
-], function (Controller) {
+	"sap/ui/core/mvc/Controller",
+    "sap/m/MessageToast"
+], function (Controller, MessageToast) {
 	"use strict";
-
 	return Controller.extend("sap.ui.demo.walkthrough.controller.App", {
-
-		onShowHello : function () {
-			// show a native JavaScript alert
-			/* eslint-disable no-alert */
-			alert("Hello World");
-			/* eslint-enable no-alert */
+		onShowHello : function () {			
+			MessageToast.show("Hello World");
 		}
 	});
-
 });
