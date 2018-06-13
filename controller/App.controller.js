df884f2c94969927ecc0d6f59f9c376503891b9b
santosh.pasupunuri <santosh.pasupunuri@vistex.com>
2018-06-13 15:34:14 +0530

    nested views

--- controller/App.controller.js
+++ controller/App.controller.js
@@ -4,12 +4,6 @@ sap.ui.define([
 ], function (Controller, MessageToast) {
     "use strict";
     return Controller.extend("sap.ui.demo.controller.App", {
-        onShowHello: function () {
-            // read msg from i18n model
-            var oBundle = this.getView().getModel("i18n").getResourceBundle();
-            var sMsg = oBundle.getText("helloMsg2");
-            // show message
-            MessageToast.show(sMsg);
-        }
+
     });
 });
