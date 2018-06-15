5f5c77c5804e398f4290eb5a43814aa70f4cafa0
santosh.pasupunuri <santosh.pasupunuri@vistex.com>
2018-06-15 19:18:46 +0530

    adding filter

--- controller/InvoiceList.controller.js
+++ controller/InvoiceList.controller.js
@@ -1,8 +1,10 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller",
     "sap/ui/model/json/JSONModel",
-    "sap/ui/demo/model/formatter"
-], function (Controller, JSONModel, formatter) {
+    "sap/ui/demo/model/formatter",
+    "sap/ui/model/Filter",
+    "sap/ui/model/FilterOperator"
+], function (Controller, JSONModel, formatter, Filter, FilterOperator) {
     "use strict";
 
     return Controller.extend("sap.ui.demo.walkthrough.controller.InvoiceList", {
@@ -12,6 +14,19 @@ sap.ui.define([
                 currency: "INR"
             });
             this.getView().setModel(oViewModel, "view");
+        },
+        onFilterInvoices: function (oEvent) {
+            var aFilter = [];
+            var sQuery = oEvent.getParameter("query");
+            if (sQuery) {
+                aFilter.push(new aFilter("ProductName", FilterOperator.Contains, sQuery))
+            }
+
+            // filter binding
+
+            var oList = this.byId("invoiceList");
+            var oBinding = oList.getBinding("items");
+            oBinding.filter(aFilter);
         }
 
     });
